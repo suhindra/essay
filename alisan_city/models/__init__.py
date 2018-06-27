c9617e51947a3087a109ef0d07a8ce2b89a1d218
suhindra <suhindra@hotmail.co.id>
2018-06-27 23:51:50 +0700

    delete unused addons

--- alisan_city/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# License, author and contributors information in:
-# __openerp__.py file at the root folder of this module.
-#
-
-from . import city
-from . import state
-from . import partner
--- base_sms_gateway/models/sms_gateway_check_status.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-class SmsGatewayCheckStatus(models.Model):
-    _name = 'sms.gateway.check.status'
-    name = fields.Char(required=True)
-    numberOfUpdates = fields.Integer('Number of updates', help='The number of times the scheduler has run and updated this field')
-    lastModified = fields.Date('Last updated')
